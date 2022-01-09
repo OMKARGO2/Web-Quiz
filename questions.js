228b3b7cc94ea7624359470937f5351d53d10d5b
OMKARGO2 <90676252+OMKARGO2@users.noreply.github.com>
2022-01-09 23:55:19 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+{
+    numb: 6,
+    question: "What OOPJ?",
+    answer: "Object Oriented Programming Java",
+    options: [
+      "Object Overview Programming Java",
+      "Object Oriented Programming Java",
+      "Object Over Programming Java",
+      "Oriented object Programming Java"
+    ]
+},
+{
+    numb: 7,
+    question: "Ozone hole refers to?",
+    answer: "hole in ozone layer",
+    options: [
+      "decrease in the ozone layer in troposphere",
+      "hole in ozone layer",
+      "decrease in thickness of ozone layer in stratosphere",
+      "increase in the thickness of ozone layer in troposphere"
+    ]
+},
+{
+    numb: 8,
+    question: "Ordinary table salt is sodium chloride. What is baking soda?",
+    answer: "Sodium bicarbonate",
+    options: [
+      "Potassium chloride",
+      "Sodium bicarbonate",
+      "Potassium hydroxide",
+      "Sodium peroxide"
+    ]
+},
+{
+    numb: 9,
+    question: "The roots of 100x2 – 20x + 1 = 0 is:",
+    answer: "1/10 and 1/10",
+    options: [
+      " 1/10 and 1/20",
+      "1/10 and 1/10",
+      "1/20 and 1/20",
+      "None of the above"
+    ]
+},
+{
+    numb: 10,
+    question: "The sum of two numbers is 27 and product is 182. The numbers are:",
+    answer: "13 and 14",
+    options: [
+      " 12 and 13",
+      "12 and 15",
+      "13 and 14",
+      "13 and 24"
+    ]
+},
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
